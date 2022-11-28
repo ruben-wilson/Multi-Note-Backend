f800ff7f1043cfd037dc30c3725bff96232ac5f4
ruben wilson <105153564+ruben-wilson@users.noreply.github.com>
2022-11-28 16:42:33 +0000

    changed .env port to PORT

--- app.js
+++ app.js
@@ -79,6 +79,6 @@ app.delete('/defaults', (req, res) => {
   
 })
 
-const PORT = process.env.port || 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`listening on ${PORT} port`));
 
